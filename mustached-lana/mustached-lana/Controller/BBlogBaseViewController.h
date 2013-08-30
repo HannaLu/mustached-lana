aa6846da620e40dc5651ea2ba7a7ec7e5d5c2fb2
Hanna Lu <hanna@nexdoor.cc>
2013-08-30 20:53:54 +0800

    add cocoaPods feature: AFNetworking, ASIHTTPRequest, SDWebImage

---
+++ mustached-lana/mustached-lana/Controller/BBlogBaseViewController.h
@@ -0,0 +1,13 @@
+//
+//  BBlogBaseViewController.h
+//  mustached-lana
+//
+//  Created by Hanna Lu on 13/8/30.
+//  Copyright (c) 2013年 Lu Yun Han. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BBlogBaseViewController : UIViewController
+
+@end
---
+++ mustached-lana/mustached-lana/Model/BBlogDataProvider.m
@@ -0,0 +1,13 @@
+//
+//  BBlogDataProvider.m
+//  mustached-lana
+//
+//  Created by Hanna Lu on 13/8/30.
+//  Copyright (c) 2013年 Lu Yun Han. All rights reserved.
+//
+
+#import "BBlogDataProvider.h"
+
+@implementation BBlogDataProvider
+
+@end
