69a07059b3879cce4eaf7fe9383abffe93614b49
Hanna Lu <hanna@nexdoor.cc>
2013-08-30 11:06:31 +0800

    create project mustached-lana

---
+++ mustached-lana/mustached-lana/BBlogAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  BBlogAppDelegate.h
+//  mustached-lana
+//
+//  Created by Hanna Lu on 13/8/30.
+//  Copyright (c) 2013年 Lu Yun Han. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BBlogAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ mustached-lana/mustached-lana/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  mustached-lana
+//
+//  Created by Hanna Lu on 13/8/30.
+//  Copyright (c) 2013年 Lu Yun Han. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "BBlogAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([BBlogAppDelegate class]));
+    }
+}
